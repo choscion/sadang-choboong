965e24933d7a3124dbacf4d70d9ed347545ae69f
choscion <whiill1111@naver.com>
2025-09-20 11:08:44 +0900

    Project Set

---
+++ apps/ui-docs/.storybook/preview.js
@@ -0,0 +1,18 @@
+import _ from "../node_modules/@choboong/ui/dist/styles/design-system.scss"
+
+// eslint-disable-next-line no-console
+console.log("_", _);
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
+
+export default preview
